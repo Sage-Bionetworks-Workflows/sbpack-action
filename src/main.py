f8c5f8095c576fef82e046533bce3d96950e8bd7
thomasyu888 <thomas.yu@sagebase.org>
2021-07-28 16:18:55 +0800

    Write config file in main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import configparser
 import os
 import subprocess
 
@@ -5,7 +6,18 @@ import subprocess
 app_name = os.getenv("INPUT_APP_NAME")
 # {user}/{project}/{app}
 workflow_path = os.getenv("INPUT_WORKFLOW_PATH")
+auth_token = os.getenv("INPUT_AUTH_TOKEN")
 
+# Write SBG config
+config = configparser.ConfigParser()
+config['sbg'] = {'api_endpoint': 'https://cavatica-api.sbgenomics.com/v2',
+                 'auth_token': auth_token}
+
+sbg_folder = os.path.expanduser("~/.sevenbridges")
+os.mkdir(sbg_folder)
+
+with open(os.path.join(sbg_folder, "credentials"), 'w') as configfile:
+    config.write(configfile)
 
 cmd = ['sbpack', 'sbg', app_name, workflow_path]
 subprocess.check_call(cmd)
